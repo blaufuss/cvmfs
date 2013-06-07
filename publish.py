7695f8ea5a1a747d98feaedce2a2cff99e278c52
David Schultz <davids24@gmail.com>
2013-06-07 19:18:43 +0000

    add versioning support.  will increment version every time it is run

--- publish.py
+++ publish.py
@@ -8,6 +8,10 @@ from contextlib import contextmanager
 
 class Publish(object):
     """Publish changes in a repository to cvmfs"""
+    data_dir = 'data'
+    ports_dir = 'ports'
+    meta_dir = 'meta-projects'
+    
     def __init__(self,input=None,output=None,config=None):
         self.input = input
         self.output = output
@@ -31,7 +35,52 @@ class Publish(object):
             subprocess.check_call(['/usr/bin/sudo','/usr/bin/cvmfs_server',
                                    'publish'])
     
+    def _publish_with_versioning(self,input_dir,output_dir):
+        """Do versioning of directory"""
+        version = 0
+        for d in os.listdir(output_dir):
+            if d.startswith('v'):
+                v = int(d[1:])
+                if v > version:
+                    version = v
+        version = 'v%06d'%version
+        
+        subprocess.check_call(['/usr/bin/rsync','-og','-i','-rlptD',
+                               '--exclude /.cvmfscatalog','--delete',
+                               input_dir,os.path.join(output_dir,version)])
+        
+        # symlink latest to the new version
+        curdir = os.getcwd()
+        try:
+            os.chdir(output_dir)
+            os.remove('latest')
+            os.symlink(version,'latest')
+        finally:
+            os.chdir(curdir)
+    
+    def _publish_data(self):
+        """Do versioning and copying of data"""
+        input_data = os.path.join(self.input,self.data_dir)
+        output_data = os.path.join(self.output,self.data_dir)
+        self._publish_with_versioning(input_data,output_data)
+    
+    def _publish_ports(self,arch):
+        """Do versioning and copying of ports"""
+        input_data = os.path.join(self.input,self.ports_dir)
+        output_data = os.path.join(self.output,self.ports_dir)
+        self._publish_with_versioning(input_data,output_data)
+    
+    def _publish_meta(self,arch):
+        """Do copying of meta-projects"""
+        input_dir = os.path.join(self.input,self.meta_dir)
+        output_dir = os.path.join(self.output,self.meta_dir)
+        
+        subprocess.check_call(['/usr/bin/rsync','-og','-i','-rlptD',
+                               '--exclude /.cvmfscatalog','--delete',
+                               input_dir,output_dir])
+    
     def publish(self):
+        """Publish a repository"""
         if not self.input or not self.output:
             self.getconf()
         
@@ -42,11 +91,22 @@ class Publish(object):
         
         with self._cvmfs_transaction():
             
-            # TODO: add versioning
+            for d in os.listdir(self.input):
+                if d == 'data':
+                    continue
+                elif os.path.isdir(d):
+                    # must be an ARCH
+                    self._publish_ports(d)
+                    self._publish_meta(d)
+                else:
+                    # random file
+                    subprocess.check_call([
+                        '/usr/bin/rsync','-og','-i','-rlptD',
+                        '--exclude /.cvmfscatalog','--delete',
+                        os.path.join(self.input,d),
+                        os.path.join(self.output,d)])
             
-            subprocess.check_call(['/usr/bin/rsync','-og','-i','-rlptD',
-                                   '--exclude /.cvmfscatalog','--delete',
-                                   self.input,self.output])
+            self._publish_data()
         
     
 if __name__ == '__main__':
