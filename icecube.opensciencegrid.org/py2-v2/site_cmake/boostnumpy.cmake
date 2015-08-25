d5a58f708b612f068199486c9bd914f7f0f24d4b
David Schultz <davids24@gmail.com>
2015-08-25 19:32:23 +0000

    remove site_cmake since we don't need it

--- icecube.opensciencegrid.org/py2-v2/site_cmake/boostnumpy.cmake
+++
@@ -1,8 +0,0 @@
-set(BOOSTNUMPY_LIBRARIES "$ENV{SROOT}/lib/libboost_numpy.so" CACHE FILEPATH "boost::numpy library")
-set(BOOSTNUMPY_INCLUDE_DIR "$ENV{SROOT}/include" CACHE PATH "Path to boost::numpy include dir")
-set(BOOSTNUMPY_FOUND TRUE CACHE BOOL "boost::numpy found")
-
-# boost::numpy can't work without the Numpy C API
-LIST(APPEND BOOSTNUMPY_INCLUDE_DIR ${NUMPY_INCLUDE_DIR})
-LIST(APPEND BOOSTNUMPY_LIBRARIES ${NUMPY_LIBRARIES})
-
