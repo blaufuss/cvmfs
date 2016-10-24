e2205029a9867887fadebadeb372c993ae5b8d6f
David Schultz <davids24@gmail.com>
2016-10-24 17:37:15 -0500

    update metaprojects

--- builders/variants/py2_v2_metaprojects.py
+++ builders/variants/py2_v2_metaprojects.py
@@ -16,26 +16,34 @@ def build(src,dest,svn_up=None,**build_kwargs):
     dir_name = os.path.join(dest,'py2-v2')
     if not os.path.isdir(dir_name):
         raise Exception('base does not exist')
-    
+
     # now, do the OS-specific stuff
     load_env(dir_name)
     if 'SROOT' not in os.environ:
         raise Exception('$SROOT not defined')
     dir_name = os.environ['SROOT']
-    
+
     kwargs = {}
     if svn_up is not None:
         kwargs['svn_up'] = svn_up
 
     # releases
     tools['i3_metaproject']['offline-software']['V15-08-00'](dir_name,**kwargs)
+    tools['i3_metaproject']['offline-software']['V16-10-00'](dir_name,**kwargs)
+
     tools['i3_metaproject']['simulation']['V05-00-00'](dir_name,**kwargs)
+    tools['i3_metaproject']['simulation']['V05-00-01'](dir_name,**kwargs)
+    tools['i3_metaproject']['simulation']['V05-00-02'](dir_name,**kwargs)
+    tools['i3_metaproject']['simulation']['V05-00-03'](dir_name,**kwargs)
+    tools['i3_metaproject']['simulation']['V05-00-04'](dir_name,**kwargs)
+
     tools['i3_metaproject']['icerec']['V05-00-00'](dir_name,**kwargs)
+    tools['i3_metaproject']['icerec']['V05-00-01'](dir_name,**kwargs)
+    tools['i3_metaproject']['icerec']['V05-00-02'](dir_name,**kwargs)
+    tools['i3_metaproject']['icerec']['V05-00-03'](dir_name,**kwargs)
+    tools['i3_metaproject']['icerec']['V05-00-04'](dir_name,**kwargs)
+    tools['i3_metaproject']['icerec']['V05-00-05'](dir_name,**kwargs)
 
     # trunks
     tools['i3_metaproject']['combo']['stable'](dir_name,**kwargs)
-    #tools['i3_metaproject']['offline-software']['trunk'](dir_name,**kwargs)
-    #tools['i3_metaproject']['simulation']['trunk'](dir_name,**kwargs)
-    #tools['i3_metaproject']['icerec']['trunk'](dir_name,**kwargs)
-    #tools['i3_metaproject']['combo']['trunk'](dir_name,**kwargs)
 
