aefc74a22f8ac684a633e8fab91158c78e255e2f
David Schultz <davids24@gmail.com>
2017-02-17 16:16:47 -0600

    apsw needs some help finding the right python

--- builders/tools/apsw.py
+++ builders/tools/apsw.py
@@ -20,12 +20,14 @@ def install(dir_name,version=None):
             wget(url,path)
             unzip(path,tmp_dir)
             apsw_dir = os.path.join(tmp_dir,'apsw-'+str(version))
+            env = dict(os.environ)
+            env['CFLAGS'] = '-I$SROOT/include/python -L$SROOT/lib'
 
             if subprocess.call(['python','setup.py','fetch','--all',
                                 '--version',sqlite_version,
                                 'build','--enable-all-extensions',
                                 'install','--prefix',dir_name],
-                                cwd=apsw_dir):
+                                cwd=apsw_dir, env=env):
                 raise Exception('apsw failed to install')
         finally:
             shutil.rmtree(tmp_dir)
