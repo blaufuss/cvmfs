c92f3d86cb57a162d7af214f0be6acd4512ba997
David Schultz <davids24@gmail.com>
2017-04-19 14:31:53 -0500

    better LDFLAGS definition

--- builders/tools/suitesparse.py
+++ builders/tools/suitesparse.py
@@ -43,8 +43,10 @@ def install(dir_name,version=None):
                     elif 'BLAS =' in line or 'LAPACK =' in line or 'CFOPENMP ' in line:
                         line = '#'+line
                     f.write(line+'\n')
+            ldflags = '-L'+os.path.join(suitesparse_dir,'lib')
+            ldflags += ' -L'+os.path.join(dir_name,'lib')
             if subprocess.call(['make', '-j', cpu_cores, 'library',
-                                'LDFLAGS=-L'+os.path.join(suitesparse_dir,'lib')],
+                                'LDFLAGS="%s"'%ldflags],
                                 cwd=suitesparse_dir):
                 raise Exception('suitesparse failed to make')
             if subprocess.call(['make','install'],cwd=suitesparse_dir):
